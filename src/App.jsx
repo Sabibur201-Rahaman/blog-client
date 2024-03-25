e94a0aa2253d572e8ff0c646aee003d1756c7d3b
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2024-03-25 15:28:39 +0530

    client part initialy created using mui

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Blog from "./pages/Blog";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Blog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
