6f74235eec8b0941c03b0d7416ad942dc2b97cf0
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2024-03-27 15:46:45 +0530

    register page is created using material ui

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import{Box,Typography,TextField,Button}from '@mui/material'
 const Register = () => {
   return (
-    <div>
-      <h3>this is register</h3>
-    </div>
+    <>
+      <Box
+      maxWidth={400}
+      display='flex'
+      flexDirection='column'
+      alignItems='center'
+      justifyContent='center'
+      margin='auto'
+      marginTop={5}
+      boxShadow='10px 10px 20px #ccc'
+      padding={3}
+      borderRadius={5}
+    >
+    <Typography variant='h4'>
+    Register
+    </Typography>
+    <TextField/>
+    <TextField/>
+    <TextField/>
+    <Button>Submit</Button>
+    <Button>Already Registered?please Login</Button>
+    </Box>
+    </>
   )
 }
 
