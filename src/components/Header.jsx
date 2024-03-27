66b13ca2057beebd5782afaaaa69aaf949deef55
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2024-03-27 14:06:14 +0530

    login,register,logout is manipulated using useselector react-redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
+import {useSelector}from 'react-redux'
 import { Box, AppBar, Toolbar, Button, Typography,Tabs,Tab} from "@mui/material";
 import { Link } from "react-router-dom";
 import Blog from './../pages/Blog';
 const Header = () => {
   const[ value,setvalue]=useState()
+  const isLogin=useSelector(state=>state.isLogin)
   return (
     <>
       <AppBar position="sticky">
         <Toolbar>
             <Typography variant="h4">BlogApp</Typography>
-            <Box display={"flex"}marginLeft="auto" marginRight={"auto"}>
+            {isLogin &&<>
+              <Box display={"flex"}marginLeft="auto" marginRight={"auto"}>
               <Tabs textColor="inherit" value={value} onChange={(e,val)=>setvalue(val)}>
             <Tab label='Blogs' LinkComponent={Link} to='/'/>
             <Tab label='My-Blogs' LinkComponent={Link} to='/my-blogs'/>
               </Tabs>
             </Box>
+            </>}
             <Box display={"flex"} marginLeft='Auto'>
-            <Button sx={{margin:1,color:"white"}} LinkComponent={Link} to='/register'>Register</Button>
+            {!isLogin && <>
+              <Button sx={{margin:1,color:"white"}} LinkComponent={Link} to='/register'>Register</Button>
             <Button sx={{margin:1,color:"white"}} LinkComponent={Link} to='/login'>Login</Button>
-            <Button sx={{margin:1,color:"white"}}>Logout</Button>
+            </>}
+            {isLogin &&(<>
+              <Button sx={{margin:1,color:"white"}}>Logout</Button>
+
+            </>)}
             </Box>
         </Toolbar>
       </AppBar>
