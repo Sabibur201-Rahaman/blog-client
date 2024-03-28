3da68f6cc06250f1c67d73a8aa2919da7a346a50
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2024-03-28 14:40:56 +0530

    user register is applied using api call

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, useNavigate } from "react-router-dom";
+import axios from 'axios'
 import{Box,Typography,TextField,Button}from '@mui/material'
 const Register = () => {
+  const navigate=useNavigate()
+  const[inputs,setInputs]=useState({
+    name:'',
+    email:'',
+    password:''
+  })
+  const handleChange=(e)=>{
+setInputs((prevState)=>({
+  ...prevState,
+  [e.target.name]:e.target.value
+}))
+  }
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    try{
+const {data}=await axios.post ('http://localhost:8080/api/v1/user/register',{
+  userName:inputs.name,
+  email:inputs.email,
+  password:inputs.password
+})
+if(data.success){
+  alert('user Successfully registered')
+  navigate('/login')
+}
+    }catch(error){
+console.log(error)
+    }
+  }
   return (
     <>
+    <form onSubmit={handleSubmit}>
       <Box
       maxWidth={400}
       display='flex'
@@ -18,12 +49,40 @@ const Register = () => {
     <Typography variant='h4'>
     Register
     </Typography>
-    <TextField/>
-    <TextField/>
-    <TextField/>
-    <Button>Submit</Button>
-    <Button>Already Registered?please Login</Button>
+    <TextField placeholder='name'
+    name='name'
+    type={'text'}
+    margin='normal'
+    required
+    onChange={handleChange}
+    />
+    <TextField placeholder='email'
+    name='email'
+    type={'email'}
+    margin='normal'
+    required
+    onChange={handleChange}
+
+    />
+    <TextField placeholder='password'
+    name='password'
+    type={'password'}
+    margin='normal'
+    required
+    onChange={handleChange}
+
+    />
+    <Button type="submit"
+    sx={{borderRadius:3,marginTop:3}}
+    color='primary'
+    variant='contained'
+    >Submit</Button>
+
+    <Button type="submit"
+    sx={{borderRadius:3,marginTop:3}}
+    color='primary' onClick={()=>navigate('/login')}>Already Registered?please Login</Button>
     </Box>
+    </form>
     </>
   )
 }
