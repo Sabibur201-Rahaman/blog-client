09a8c1e3723926d2745ddf399fe3e3635ccd0382
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2024-04-02 16:03:10 +0530

    logout is applied & get all the blogs also applied by using api called

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,75 @@
 import React, { useState } from "react";
-import {useDispatch, useSelector}from 'react-redux'
-import { Box, AppBar, Toolbar, Button, Typography,Tabs,Tab} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  Tabs,
+  Tab,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { authActions } from "../../redux/store";
 const Header = () => {
-  const[ value,setvalue]=useState()
-  const isLogin=useSelector(state=>state.isLogin)
-  const dispatch=useDispatch()
-  const navigate=useNavigate()
-  const handleLogout=()=>{
-    dispatch(authActions.Logout())
-    navigate('/login')
-  }
+  const [value, setvalue] = useState();
+  const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(authActions.Logout());
+    navigate("/login");
+  };
   return (
     <>
       <AppBar position="sticky">
         <Toolbar>
-            <Typography variant="h4">BlogApp</Typography>
-            {isLogin &&<>
-              <Box display={"flex"}marginLeft="auto" marginRight={"auto"}>
-              <Tabs textColor="inherit" value={value} onChange={(e,val)=>setvalue(val)}>
-            <Tab label='Blogs' LinkComponent={Link} to='/'/>
-            <Tab label='My-Blogs' LinkComponent={Link} to='/my-blogs'/>
-              </Tabs>
-            </Box>
-            </>}
-            <Box display={"flex"} marginLeft='Auto'>
-            {!isLogin && <>
-              <Button sx={{margin:1,color:"white"}} LinkComponent={Link} to='/register'>Register</Button>
-            <Button sx={{margin:1,color:"white"}} LinkComponent={Link} to='/login'>Login</Button>
-            </>}
-            {isLogin &&(<>
-              <Button sx={{margin:1,color:"white"}} onClick={handleLogout}>Logout</Button>
-
-            </>)}
-            </Box>
+          <Typography variant="h4">BlogApp</Typography>
+          {isLogin && (
+            <>
+              <Box display={"flex"} marginLeft="auto" marginRight={"auto"}>
+                <Tabs
+                  textColor="inherit"
+                  value={value}
+                  onChange={(e, val) => setvalue(val)}
+                >
+                  <Tab label="Blogs" LinkComponent={Link} to="/" />
+                  <Tab label="My-Blogs" LinkComponent={Link} to="/my-blogs" />
+                </Tabs>
+              </Box>
+            </>
+          )}
+          <Box display={"flex"} marginLeft="Auto">
+            {!isLogin && (
+              <>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/register"
+                >
+                  Register
+                </Button>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/login"
+                >
+                  Login
+                </Button>
+              </>
+            )}
+            {isLogin && (
+              <>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  onClick={handleLogout}
+                >
+                  Logout
+                </Button>
+              </>
+            )}
+          </Box>
         </Toolbar>
       </AppBar>
     </>
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import BlogCard from "./../components/BlogCard";
 const Blog = () => {
-  return (
-    <div>
-      hello thi blog
-    </div>
-  )
-}
+  const [blogs, setBlogs] = useState([]);
+  const getAllBlocks = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8080/api/v1/blog/allBlog"
+      );
+      console.log(data);
+      if (data?.success) {
+        setBlogs(data?.blogs);
+      }
+    } catch (error) {
+      console.log("error here::::", error);
+    }
+  };
+  useEffect(() => {
+    getAllBlocks();
+  }, []);
+  return <div>{blogs && blogs.map((blog) => <BlogCard  
+  title={blog.title}
+  description={blog.description}
+  image={blog.image}
+  userName={blog.user.userName}
+  time={blog.createdAt}
+  />)}</div>;
+};
 
-export default Blog
+export default Blog;
